bb111f63fc09bdf2022fe661389aa03732eacba2
Indyka Saidi <indyka@outlook.com>
2018-05-21 18:39:10 +0700

    first commit

---
+++ bot.js
@@ -0,0 +1,57 @@
+var Twit = require('twit')
+var Giphy = require('giphy');
+var async = require('async');
+var request = require('request');
+var fs = require('fs');
+var sys = require('util')
+var exec = require('child_process').exec;
+
+require('dotenv').config();
+
+var giphy = new Giphy(process.env.giphyKey);
+
+var T = new Twit({
+  consumer_key: process.env.consumerKey,
+  consumer_secret: process.env.consumerSecret,
+  access_token: process.env.accessToken,
+  access_token_secret: process.env.accessTokenSecret,
+  timeout_ms: 60*1000,
+});
+
+var streamT = T.stream('lists/statuses', { slug: 'team-t', owner_screen_name: 'officialjkt48' });
+
+streamT.on('tweet', function (tweet) {
+  console.log(tweet.screen_name + ': ' + tweet.text);
+  keyword = tweet.text.split(" ");
+  giphy.translate({s: keyword[Math.floor(Math.random() * keyword.length)]}, function(error, gif, res){
+    url = "https://media.giphy.com/media/" + gif.data.id + "/giphy.gif";
+    
+    request({
+      method: 'GET',
+      url: url,
+      encoding: 'binary'
+    }, function(error, response, body){
+
+      var b = new Buffer(body.toString(), 'binary');
+      image = b.toString('base64');
+
+      T.post('media/upload', { media_data: image }, function (err, data, response) {
+        var mediaIdStr = data.media_id_string
+        var meta_params = { media_id: mediaIdStr }
+        
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+          if (!err) {  
+            T.post('statuses/update', { status: '@' + tweet.user.screen_name, in_reply_to_status_id: tweet.id_str, media_ids: [mediaIdStr] }, function(err, data, response) {
+              console.log(data)
+            });
+          } else {
+            console.log('error cuk');
+          }
+        })
+      })
+    });
+  });
+});
+
+
+
